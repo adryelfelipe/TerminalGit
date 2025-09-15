5cfaed08494967ccf21c19f2ffe1802bd463c5b9
Adryel Felipe <adryxd003@gmail.com>
2025-09-15 09:36:54 -0300

    Terceiro commit antes de dar o pull, oq pode acontecer? Talvez um merge inusitado

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,7 @@ public class Main {
         System.out.print("Primeiro commit no git!");
 
         System.out.println("Partindo para o segundo commit!");
+
+        System.out.println("Terceiro commit, porém tenho que dar pull para add o readme, vou ver no que dá");
     }
 }
