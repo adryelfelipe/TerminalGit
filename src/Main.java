c8dc71056c24207bfd160e09c23b8de219e94f85
Adryel Felipe <adryxd003@gmail.com>
2025-09-15 09:49:57 -0300

    Atualiza Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.printf("Primeiro commit no git!");
+        System.out.print("Primeiro commit no git!");
 
+        System.out.println("Partindo para o segundo commit!");
     }
 }
